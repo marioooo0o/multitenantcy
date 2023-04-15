a209bc2dd421428549a1e082c7a5e1ea5404589a
Mariusz Dąbrowski <57808812+marioooo0o@users.noreply.github.com>
2023-04-15 19:19:43 +0200

    update

--- routes/api.php
+++ routes/api.php
@@ -16,5 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/login', function (Request $request) {
-    return Tenant::where('id', $request->id);
+    return Tenant::where('id', $request->id)->first();
 });
